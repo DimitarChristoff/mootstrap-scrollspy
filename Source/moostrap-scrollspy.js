55996acd77ac22a5d8defb236a7fdf369e569cc0
DimitarChristoff <christoff@gmail.com>
2012-02-06 10:26:31 +0000

    initial commit through arian's port of twitter bootstrap scroll spy

---
+++ Source/moostrap-scrollspy.js
@@ -0,0 +1,52 @@
+(function() {
+
+    var moostrapScrollspy = this.mootsrapScrollspy = new Class({
+
+        initialize: function(element, wrapper){
+            element = this.element = document.id(element);
+            this.wrapper = wrapper || window;
+
+            var links = this.links = element.getElements('a');
+            var elements = this.elements = [];
+
+            links.each(function(el){
+                var href = el.get('href'), target;
+                if (href.slice(0, 1) == '#') target = document.id(el.get('href').slice(1));
+                elements.push(target);
+            });
+
+            this.attach();
+        },
+
+        attach: function(){
+            if (!this.boundScroll) this.boundScroll = this.scroll.bind(this);
+            this.wrapper.addEvent('scroll', this.boundScroll);
+        },
+
+        detach: function(){
+            if (this.boundScroll) this.wrapper.removeEvent('scroll', this.boundScroll);
+        },
+
+        scroll: function(){
+            var top = this.wrapper.getScroll().y;
+            var last, index;
+            this.elements.some(function(el, i){
+                if (!el) return false; // skip empty entry
+                var y = el.getPosition(this.wrapper == window ? document.body : this.wrapper).y;
+                if (y < top) index = i;
+                return y >= top;
+            }, this);
+
+            if (index != this.active){
+                if (this.active != null) this.links[this.active].removeClass('active');
+                this.active = index;
+                if (index != null) this.links[index].addClass('active');
+            }
+
+        }
+
+    });
+
+
+
+})();
